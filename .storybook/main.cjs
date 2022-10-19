f7065b00b9b07e4c4345163437901509b4183e5e
Artur Diego <arturdiegodev@gmail.com>
2022-10-18 22:21:14 -0300

    MSW, Storybook Runner Addon, SignIn page for test with MSW

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -1,21 +1,25 @@
 module.exports = {
-    stories: [
+    "stories": [
         '../src/**/*.stories.mdx',
         '../src/**/*.stories.@(js|jsx|ts|tsx)',
     ],
-    addons: [
+    "addons": [
         '@storybook/addon-links',
         '@storybook/addon-essentials',
         '@storybook/addon-interactions',
         '@storybook/addon-a11y'
     ],
-    framework: '@storybook/react',
-    core: {
-        builder: '@storybook/builder-vite',
+    "framework": '@storybook/react',
+    "core": {
+        "builder": '@storybook/builder-vite',
     },
-    features: {
-        storyStoreV7: true,
+    "features": {
+        "storyStoreV7": true,
+        "interactionsDebugger": true,
     },
+    "staticDirs": [
+        "../public"
+    ],
     viteFinal: (config, { configType }) => {
         if (configType === 'PRODUCTION') {
           config.base = '/design-system-ignite-lab/'
